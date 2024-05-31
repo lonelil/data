503a12d9e256396e74aae369294d5bfdf48ff449
lonelil <51315646+lonelil@users.noreply.github.com>
2024-05-31 19:27:12 +0800

    add scripts

---
+++ src/applemusic.ts
@@ -0,0 +1,25 @@
+import { ofetch } from "ofetch";
+
+const apple = ofetch.create({
+  baseURL: "https://beta.music.apple.com",
+});
+
+const home = (await apple("/")) as string;
+
+const index_js_path = home.match(
+  /\/assets\/index-legacy-[a-f0-9]+\.js/
+)?.[0] as string;
+
+console.log("got index.js path", index_js_path);
+
+const index_js = await apple(index_js_path, {
+  parseResponse(responseText) {
+    return responseText;
+  },
+});
+
+const token = index_js.match(/(?=eyJh)(.*?)(?=")/)?.[0];
+
+console.log("got token", token);
+
+Bun.write("./data/token.txt", token);
